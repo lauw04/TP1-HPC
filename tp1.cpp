189e7ca570c0d5ef474b73cd4ff0c5c02e71526e
Lauren Picard <lauren.picard@insa-lyon.fr>
2019-02-28 09:38:28 +0100

    q3 non testée

--- tp1.cpp
+++ tp1.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 vector<double> q1(int n);
+vector<double> q3(vector<double> v1,vector<double> v2);
 
 vector<double> q1(int n){
   vector<double> v;
@@ -18,14 +19,24 @@ vector<double> q1(int n){
   return v;
 }
 
+vector<double> q3(vector<double> v1,vector<double> v2){
+  vector<double> somme;
+  if(v1.size() == v2.size()){
+    for(int i = 0; i < size(v1); i++){
+      somme.push_back(v1[i] + v2[i])
+    }
+  }
+  else{
+    cout << "Les vecteurs ne font pas la même taille !" << endl;}
+  return somme
+}
+
 
 int main(){
   cout << "Hello World!" << endl;
   vector<double> v1 = q1(10);
-  vector<double>::iterator it; 
-  for(it = v1.begin(); it!=v1.end(); ++it)
-    {
-        cout << *it << endl;
-    }
+  vector<double> v2 = q1(10);
+  vector<double> v3 = q3(v1,v2);
+  q2(v3);
   return 0;
 }
