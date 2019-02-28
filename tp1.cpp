36d3308fed2ca519a8cf9de6283471dad7cfea91
Lauren Picard <lauren.picard@insa-lyon.fr>
2019-02-28 08:56:34 +0100

    q1

--- tp1.cpp
+++ tp1.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void q1(int n);
+
+void q1(int n){
+  vector<double> v;
+  double nb_aleatoire = 0;
+  for(int i = 0; i < n; i++)
+     {
+        nb_aleatoire = rand();
+        cout << nb_aleatoire << endl;
+        v.push_back(nb_aleatoire);
+     }
+}
+
+
 int main(){
   cout << "Hello World!" << endl;
+  q1(10);
   return 0;
 }
