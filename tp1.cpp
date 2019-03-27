584efbccfb60334f05b9aabc0aa734f8e6255108
J.Antoine <justineantoine17@gmail.com>
2019-03-27 15:19:44 +0100

    On va dire que c'est fini

--- tp1.cpp
+++ tp1.cpp
@@ -50,7 +50,7 @@ vector<double> q3(vector<double> v1,vector<double> v2){
 	#pragma omp parallel
 	{
 		if(v1.size() == v2.size()){
-		#pragma omp for
+			#pragma omp for
 			for(int i = 0; i < v1.size(); i++){
 				addition[i]=(v1[i] + v2[i]);
 			}
@@ -169,15 +169,14 @@ vector<double> q11(vector<vector<double>> m, vector<double> v){
 	size_t p = m[0].size();
 	size_t k = v.size();
 	vector<double> result;
+	result.resize(n);
 	#pragma omp parallel
 	{
 		if (p==k){
-			result.resize(n);
 			#pragma omp for
 			for (int i=0; i<n; i++){
 				result[i] = 0;
 				for (int j=0; j<p; j++){
-					cout << m[i][j] << " " << v[j] << endl;
 					result[i] += m[i][j]*v[j];
 				}
 			}
@@ -195,18 +194,17 @@ vector<vector<double>> q12(vector<vector<double>> m1, vector<vector<double>> m2)
 	size_t n2 = m2.size();
 	size_t p2 = m2[0].size();
 	vector<vector<double>> result;
+	result.resize(n1);
+	for (int a=0;a<n1;a++){
+		result[a].resize(p2);
+	}
 	#pragma omp parallel
 	{
 		if (p1==n2){
-			result.resize(n1);
-			for (int a=0; a<n1; a++){
-				result[a].resize(p2);
-			}
 			#pragma omp for
 			for (int i=0; i<n1; i++){
 				for (int j=0; j<p2; j++){
 					result[i][j]=0;
-					cout << endl;
 					for (int k=0; k<p1;k++){
 						result[i][j] += m1[i][k]*m2[k][j];
 					}	
@@ -227,12 +225,14 @@ int main(){
 	//cout << "Hello, World" << endl;
   
 	int num_threads;
-	cout << "Combien de coeurs on utilise ?" << endl;
-	cin >> num_threads;
-	omp_set_num_threads(num_threads);
 	char type;
 	cout << "Pour travailler avec des vecteurs tapez 'v'. Pour travailler avec des matrices, tapez 'm'. Pour tester les fonctions d'affichages, tapez 't'" << endl;
 	cin >> type;
+	cout << "Combien de coeurs on utilise ?" << endl;
+	cin >> num_threads; 
+	omp_set_num_threads(num_threads);
+	
+	
 	
 	if (type=='v'){
 		int n;
@@ -241,23 +241,22 @@ int main(){
 		vector<double> v1 = q1(n);
 		vector<double> v2 = q1(n);
 		chrono::time_point<chrono::system_clock> start, end;
-  
+
 		/* ADDITION */
   
 		start = chrono::system_clock::now();  
 		vector<double> addition = q3(v1, v2);
 		end = chrono::system_clock::now();
 		int time_addition = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << " Temps d'éxécution de l'addition de deux vecteurs de taille " << n << " avec " << num_threads << " coeur(s) : " << time_addition << endl;
+		cout << " Temps d'éxécution de l'addition de deux vecteurs de taille " << n << " : " << time_addition << endl;
 
-		/* SOMME */
+		 /* SOMME */
 	
 		start = std::chrono::system_clock::now();  
 		double somme = q4(v1);
 		end = chrono::system_clock::now();
 		int time_somme = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << "Temps d'éxécution de la somme des éléments d'un vecteur de taille " << n << " avec " << num_threads << " coeur(s) : " << time_somme << endl;
-  
+		cout << "Temps d'éxécution de la somme des éléments d'un vecteur de taille " << n << " : " << time_somme << endl;
   
 		/* MULTIPLICATION */
 		
@@ -269,7 +268,7 @@ int main(){
 		vector<double> multi = q8(v1,x);
 		end = chrono::system_clock::now();
 		int time_multi = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << "Temps d'éxécution de la multiplication d'un vecteur de taille " << n << " par un scalaire, avec " << num_threads << " coeur(s) : " << time_multi << endl;
+		cout << "Temps d'éxécution de la multiplication d'un vecteur de taille " << n << " par un scalaire : " << time_multi << endl;
 	}
 	
 	else if (type == 'm'){
@@ -284,14 +283,14 @@ int main(){
 		vector<vector<double>> m3 = q10_1(m,n);
 		vector<double> v = q1(m);
 		chrono::time_point<chrono::system_clock> start, end;
-		
+	
 		/* ADDITION */
 		
 		start = chrono::system_clock::now();  
 		vector<vector<double>> addition = q10_3(m1, m2);
 		end = chrono::system_clock::now();
 		int time_addition = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << " Temps d'éxécution de l'addition de deux matrices de taille " << n << " x " << m << "avec" << num_threads << " coeur(s) : " << time_addition << endl;
+		cout << " Temps d'exécution de l'addition de deux matrices de taille " << n << " x " << m << " : " << time_addition << endl;
 		
 		/* SOMME */
 
@@ -299,7 +298,7 @@ int main(){
 		double somme = q10_4(m1);
 		end = chrono::system_clock::now();
 		int time_somme = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << "Temps d'éxécution de la somme des éléments d'une matrice de taille " << n << " x " << m << " avec " << num_threads << " coeur(s) : " << time_somme << endl;
+		cout << "Temps d'exécution de la somme des éléments d'une matrice de taille " << n << " x " << m << "  : " << time_somme << endl;
   
 		/* MULTIPLICATION */
 		
@@ -310,7 +309,7 @@ int main(){
 		vector<vector<double>> multi = q10_5(m1,x);
 		end = chrono::system_clock::now();
 		int time_multi = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << "Temps d'éxécution de la multiplication d'une matrice de taille " << n << "x" << m << " par un scalaire, avec " << num_threads << " coeur(s) : " << time_multi << endl;
+		cout << "Temps d'exécution de la multiplication d'une matrice de taille " << n << "x" << m << " par un scalaire : " << time_multi << endl;
 	
 		/* MULTIPLICATION MATRICE/VECTEUR*/
 		
@@ -318,7 +317,7 @@ int main(){
 		vector<double> matvec = q11(m1,v);
 		end = chrono::system_clock::now();
 		int time_matvec = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << "Temps d'éxécution de la multiplication d'une matrice et  de taille " << n << "x" << m << " par un vecteur de taille " << m << " avec " << num_threads << " coeur(s)." << endl;
+		cout << "Temps d'exécution de la multiplication d'une matrice et  de taille " << n << "x" << m << " par un vecteur de taille " << m << " : " << time_matvec << endl;
 	
 		/* MULTIPLICATION MATRICE/MATRICE*/
 
@@ -326,7 +325,7 @@ int main(){
 		vector<vector<double>> matmat = q12(m1,m3);
 		end = chrono::system_clock::now();
 		int time_matmat = chrono::duration_cast<chrono::microseconds> (end-start).count();
-		cout << "Temps d'éxécution de la multiplication de 2 matrices de taille " << n << "x" << m << "  et " << m << "x" << n << " avec " << num_threads << " coeur(s)." << endl;
+		cout << "Temps d'exécution de la multiplication de 2 matrices de taille " << n << "x" << m << "  et " << m << "x" << n << " : " << time_matmat <<  endl;
 	}
 	
 	else if (type == 't'){
@@ -348,10 +347,6 @@ int main(){
 		vector<vector<double>> v3 = q10_1(p,m);
 		q10_2(v2);
 		q10_2(v3);
-		
-		//vector<vector<double>> v4 = q12(v2,v3);
-		//q10_2(v4);
-
 	}
 
   return 0;
